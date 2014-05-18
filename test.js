65b435f90eab1a395c13cad811436fa64a0b3cce
Edward Lee <theelee13@gmail.com>
2014-05-18 02:00:13 -0400

    Added example and test MARTA data

--- test.js
+++ test.js
@@ -6,4 +6,5 @@ gtfs.convert('./data/agency.txt','mongodb://localhost:3001/meteor','agency',func
 /*
 	3001 is the default port for meteor, while mongo usually uses 27017.
 	This module can also convert other forms of CSV into mongoDB databases.
+	This example assumes a MongoDB database is already running at localhost:3001, with name meteor.
 */
