7d56e7b6b98a2175f413dc2b6d5e2f9becdd87a6
Edward Lee <theelee13@gmail.com>
2014-05-18 01:58:31 -0400

    New, node-idiomatically correct module. Makeover

--- gtfs2mongo.js
+++ gtfs2mongo.js
@@ -1,99 +1,35 @@
 var Converter=require("csvtojson").core.Converter;
 var MongoClient = require('mongodb').MongoClient;
-var url;					//global. Must be set with setURL.	
-var dest='./data';	//dest is the folder's location (containing the GTFS data) in reference to the calling method. Default is './data'
 
 /*
 	Author: William Edward Lee. Intern@Cycore Systems; cycoresys.com. github: theelee13
 */
 
 module.exports={
-	//call this before calling any method (besides convert)
-	setURL: function (turl){
-		url=turl;
-	},
-
-	setGTFSFolder: function (ndest){
-		dest=ndest;
-	},
-
-	getAgency: function (){
-		getData(dest+'/agency.txt','agency');
-	},
-
-	getCalendar: function (){
-		getData(dest+'/calendar.txt','calendar');
-	},
-
-	getCalendarDates: function (){
-		getData(dest+'/calendar_dates.txt','calendardates');
-	},
-
-	getRoutes: function (){
-		getData(dest+'/routes.txt','routes');
-	},
-
-	getShapes: function (){
-		getData(dest+'/shapes.txt','shapes');	
-	},
-
-	getStopTimes: function (){
-		getData(dest+'/stop_times.txt','stoptimes'); 
-	},
-
-	getStops: function (){
-		getData(dest+'/stops.txt','stops');
-	},
-
-	getTrips: function (){
-		getData(dest+'/trips.txt','trips');
-	},
-
-	getFareRules: function (){
-		getData(dest+'/fare_rules.txt','farerules');
-	},
-
-	getFareAttributes: function (){
-		getData(dest+'/fare_attributes.txt','fareattributes');
-	},
-
-	getFrequency: function (){
-		getData(dest+'/frequencies.txt','frequencies');
-	},
-
-	getTransfers: function (){
-		getData(dest+'/transfers.txt','transfers');
-	},
-	
-	getFeedInfo: function (){
-		getData(dest+'/feed_info.txt','feedinfo');
-	}
-}
-
-/*
-   getData is the heart of the library, as it performs all functionality.
-It works by converting the destination provided csv file to a JSON, and then
-connecting to a MongoDB database (specified in URL), where it will delete
-the named collection and create a new collection with its name and the JSON data.
-
-*/
-
-getData = function (loc,cName){
-	var csvConverter=new Converter();
-	csvConverter.on("end_parsed",function(jsonObj){
-		var getstoptimeData = jsonObj["csvRows"];
-		MongoClient.connect(url, function(err, db) {
-			if(err) throw err;
-			db.dropCollection(cName, function(err,res){
-				if(err) throw err;
-			});
-			var collection = db.collection(cName);
-			collection.insert(getstoptimeData, function(err,record){
-				if(err) throw err;
-				console.log('Recorded _id: '+record[0]._id);	//provide the ObjectID variable to console.
-				db.close();
+	convert:  function (loc,DBI,cName,callback){
+		var csvConverter=new Converter();
+		csvConverter.on("end_parsed",function(jsonObj){
+			var data = jsonObj["csvRows"];
+			MongoClient.connect(DBI, function(err, db) {
+				if(err){
+					return callback(err);
+				}
+				db.dropCollection(cName, function(err,res){
+					if(err){
+						console.log("WARNING: If creating a new collection, ignore the following error:")
+						console.log(err);
+					}
+				});
+				var collection = db.collection(cName);
+				collection.insert(data, function (err,record){
+					if(err){
+						return callback(err);
+					}
+					db.close();
+					return callback(null,record[0]._id);
+				});
 			});
 		});
-	});
-	csvConverter.from(loc);
-};
+		csvConverter.from(loc);
+	}
+}
