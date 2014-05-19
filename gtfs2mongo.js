a34011f29af3bdd0578c97679a93e0c16c3ec149
Edward Lee <theelee13@gmail.com>
2014-05-19 19:34:27 -0400

    Working version of core. Updating params now.

--- gtfs2mongo.js
+++ gtfs2mongo.js
@@ -1,7 +1,6 @@
 var parse = require('csv-parse');
 var MongoClient = require('mongodb').MongoClient;
 var fs = require('fs');
-var stream = require('stream');
 /*
 	Author: William Edward Lee. Intern@Cycore Systems; cycoresys.com. github: theelee13
 */
@@ -16,44 +15,31 @@ module.exports={
 			}
 			var collection = db.collection(cName);
 			var parser = parse({ columns:true});	//columns interprets first line as set of fields.
-			parser.on('readable',function(){
-				console.log('hi');
-				record = parser.read()
-				if(record===null){
-					if(endFlag){
-						console.log('endedFlag');
-						db.close()
+			parser.on('data',function(record){
+				parser.pause();
+				put(record,upsertID,db,collection,function(err){
+					if(err){
+						db.close();
+						return callback(err);
 					}
-					console.log('record==null');
-					return callback(null);
-				}
-				if(record){
-					put(record,upsertID,db,collection, function(err){
-						if(err){
-							db.close();
-							console.log('fail to update');
-							return callback(err);
-						}
-					});
-					console.log('pre-emit');
-				}
+					if(endFlag){
+						return callback(db.close());
+					}
+					parser.resume();
+				});
 			});
 			parser.on('error', function(err){
 				return callback(err);
 			});
 			parser.on('end',function(){
-				console.log('end received');
 				endFlag=true;
 			});
 			filestream.pipe(parser);
 		});
 	}
 }
-put = function (rec,uID,database,collection,callback){
-	collection.update({uID: rec[uID]},rec, function (err,record){
-		if(!err){
-		console.log('successfully written');
-		}
+put = function (rec,match,database,collection,callback){
+	collection.update(match,rec,{upsert:true}, function (err,record){
 		return callback(err);
 	});
 }
